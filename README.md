864fce2ef5c25718f69636eb6a00e1be59ea14fe
voylin <0voylin0@gmail.com>
2024-04-14 16:41:04 +0900

    Basic project setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gozen_lite
+# Gozen Lite
+
 Video player in Godot with FFmpeg
+
+## Part 1
+
+In part 1 of the tutorial series we only setup the project structure and the basic layout inside of the Godot project. We also created a git repository.
---
+++ src/main.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var timeline: HSlider = $VBox/Timeline
+@onready var texture: TextureRect = $VBox/Panel/TextureRect
+
+
+var is_playing: bool = false
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(a_delta: float) -> void:
+	pass
+
+
+func _on_play_pause_button_pressed():
+	is_playing = !is_playing
+	if is_playing:
+		pass
+	else:
+		pass
