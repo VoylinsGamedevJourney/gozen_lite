a3d8b88786dae939e04b79be14be34b2448aa0d2
voylin <0voylin0@gmail.com>
2024-06-13 12:52:51 +0900

    Adding main ui framework

--- src/autoloads/input_handler.gd
+++ src/autoloads/input_handler.gd
@@ -7,7 +7,7 @@ func _input(a_event: InputEvent) -> void:
 		Project.save_project()
 	if a_event.is_action_pressed("project_new_save"):
 		Project.save_project("")
-	if a_event.is_action_pressed("project_load"):
+	if a_event.is_action_pressed("project_open"):
 		print("Not implemented yet!")
 	if a_event.is_action_pressed("project_new"):
 		print("Not implemented yet!")
