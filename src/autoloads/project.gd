219cdced8f680fcef8fe6a9bb0213955c2d3ccdb
voylin <0voylin0@gmail.com>
2024-07-05 09:53:26 +0900

    Autoloads update - data structure

--- src/autoloads/project.gd
+++ src/autoloads/project.gd
@@ -8,17 +8,15 @@ signal _on_file_added(file_id: int)
 
 var _path: String = ""
 var _file_data: Dictionary = {}
+var _track_data: Array[Array] = []
 
 var file_id: int = 0
 var file_data: Dictionary = {}
 
-var tracks_video: Array = [] # Inside Array are dictionaries 
-var tracks_audio: Array = [] # Key = frame number, Value = clip id
+var tracks: Array[Dictionary] = [] # Inside Array are dictionaries. Key = frame number, Value = clip id
 
-var clip_video_id: int = 0
-var clip_audio_id: int = 0
-var clips_video: Dictionary = {}
-var clips_audio: Dictionary = {}
+var clip_id: int = 0
+var clips: Dictionary = {}
 
 
 
@@ -29,13 +27,21 @@ func _ready() -> void:
 			load_project(l_arg)
 			break
 	get_window().files_dropped.connect(_on_files_dropped)
+	
+	# Preparing tracks
+	for _i: int in Settings.default_tracks:
+		tracks.append({})
+		_track_data.append([])
 
 
 func load_project(a_path: String) -> void:
 	_path = a_path
+	tracks = []
+	_track_data = []
 	load_data(_path)
 	_on_project_loaded.emit()
 	%TimelinePanel.load_project()
+	# TODO: Load _track_data
 
 
 func save_project(a_path: String = _path) -> void:
@@ -58,39 +64,58 @@ func _on_files_dropped(a_files: PackedStringArray) -> void:
 		add_file(l_file)
 
 
-func _get_next_id() -> int:
+func _get_next_file_id() -> int:
 	file_id += 1
 	return file_id - 1
 
 
+func _get_next_clip_id() -> int:
+	clip_id += 1
+	return clip_id - 1
+
+
 func add_file(a_file_path: String) -> void:
-	var l_id: int = _get_next_id()
+	var l_id: int = _get_next_file_id()
 	file_data[l_id] = File.create_file(a_file_path)
 	match file_data[l_id]:
 		File.VIDEO:
 			_file_data[l_id] = Video.new()
 			_file_data[l_id].open_video(a_file_path)
+			file_data[l_id].duration = _file_data[l_id].get_total_frame_nr() 
 		File.IMAGE: 
 			_file_data[l_id] = Image.load_from_file(a_file_path)
 		File.AUDIO:
 			_file_data[l_id] = load(a_file_path)
+			file_data[l_id].duration = _file_data[l_id].get_length() # This is in seconds and not frames
+	file_data[l_id].nickname = a_file_path.split('/')[-1]
 	_on_file_added.emit(l_id)
 
 
 func add_color_file(a_color: Color) -> void:
-	var l_id: int = _get_next_id()
+	var l_id: int = _get_next_file_id()
 	file_data[l_id] = File.create_color_file(a_color)
+	file_data[l_id].nickname = "Color-%s" % l_id
 	_on_file_added.emit(l_id)
 
 
-func add_text_file(a_tree_location: String, a_text: String) -> void:
-	var l_id: int = _get_next_id()
+func add_text_file(a_text: String) -> void:
+	var l_id: int = _get_next_file_id()
 	file_data[l_id] = File.create_text_file(a_text)
+	file_data[l_id].nickname = "Text-%s" % l_id
 	_on_file_added.emit(l_id)
 
 
-func add_gradient_file(a_tree_location: String, a_gradient: GradientTexture2D) -> void:
-	var l_id: int = _get_next_id()
+func add_gradient_file(a_gradient: GradientTexture2D) -> void:
+	var l_id: int = _get_next_file_id()
 	file_data[l_id] = File.create_gradient_file(a_gradient)
+	file_data[l_id].nickname = "Gradient-%s" % l_id
 	_on_file_added.emit(l_id)
 
+
+func add_clip(a_clip: Clip, a_track_id: int) -> int:
+	var l_id: int = _get_next_clip_id()
+	clips[l_id] = a_clip
+	tracks[a_track_id][a_clip.timeline_start] = l_id
+	_track_data[a_track_id].append_array(range(a_clip.timeline_start, a_clip.timeline_start + a_clip.duration))
+	return l_id
+	
--- src/autoloads/settings.gd
+++ src/autoloads/settings.gd
@@ -4,8 +4,13 @@ extends DataManager
 const PATH: String = "user://settings"
 
 
-var default_video_tracks: int = 3
-var default_audio_tracks: int = 3
+var default_tracks: int = 6
+
+var duration_image: int = 150
+var duration_text: int = 150
+var duration_color: int = 150
+var duration_gradient: int = 150
+
 
 
 func _ready():
