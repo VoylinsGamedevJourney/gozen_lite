814a3b76ab3413a2bff26e90d25d1baafe311ed9
voylin <0voylin0@gmail.com>
2024-08-10 21:19:39 +0900

    Update to the Settings Handler

--- src/autoloads/settings.gd
+++ src/autoloads/settings.gd
@@ -6,13 +6,21 @@ const PATH: String = "user://settings"
 
 var default_tracks: int = 6
 
-var duration_image: int = 150
-var duration_text: int = 150
-var duration_color: int = 150
-var duration_gradient: int = 150
+var default_duration_image: int = 150:
+
+	set(a_value): set_duration_image(a_value)
+var default_duration_text: int = 150:
+	set(a_value): set_duration_text(a_value)
+var default_duration_color: int = 150:
+	set(a_value): set_duration_color(a_value)
+var default_duration_gradient: int = 150:
+	set(a_value): set_duration_gradient(a_value)
 
 var timeline_scale_max: float = 5.6
 var timeline_scale_min: float = 0.1
+
+var action_size: int = 100 # Redo and undo limit
+
 
 
 func _ready():
@@ -20,3 +28,32 @@ func _ready():
 		load_data(PATH)
 	else:
 		save_data(PATH)
+
+
+func set_duration_image(a_value: int) -> void:
+	if a_value < 0:
+		default_duration_image = 1
+		return
+	default_duration_image = a_value
+
+
+func set_duration_text(a_value: int) -> void:
+	if a_value < 0:
+		default_duration_text = 1
+		return
+	default_duration_text = a_value
+
+
+func set_duration_color(a_value: int) -> void:
+	if a_value < 0:
+		default_duration_color = 1
+		return
+	default_duration_color = a_value
+
+
+func set_duration_gradient(a_value: int) -> void:
+	if a_value < 0:
+		default_duration_gradient = 1
+		return
+	default_duration_gradient = a_value
+
